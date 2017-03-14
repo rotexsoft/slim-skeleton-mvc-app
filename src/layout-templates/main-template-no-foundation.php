216618a3bf20ca1dc9029d17fd5a672c125d9f93
Rotimi Ade <rotexdegba007-github@yahoo.ca>
2017-03-13 19:59:42 -0600

    Updated README.md, foundation and laid ground work for enabling / disabling foundation after composer create-project

--- src/layout-templates/main-template-no-foundation.php
+++ src/layout-templates/main-template-no-foundation.php
@@ -1,17 +1,16 @@
 <!doctype html>
-<html class="no-js" lang="en" dir="ltr">
+<html class="no-js" lang="en">
     <head>
-        <meta charset="utf-8">
-        <meta http-equiv="x-ua-compatible" content="ie=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>Site Title Goes Here</title>
-        <link rel="stylesheet" href="<?php echo s3MVC_MakeLink('/css/foundation/foundation.css'); ?>" />
+        <link rel="stylesheet" href="<?php echo s3MVC_MakeLink('/css/app.css'); ?>" />
     </head>
     <body>
         <div class="row">
             <div class="large-12 columns">
                 <h1>Welcome to Your New Site</h1>
-                <p>This site is powered by the <a href="https://github.com/rotexsoft/slim3-skeleton-mvc-app">SlimPHP 3 Skeleton MVC App Micro-Framework</a> based on SlimPHP 3. It also ships with the <a href="http://foundation.zurb.com/">Foundation</a> UI framework. Everything you need to know about using the Foundation UI framework can be found <a href="http://foundation.zurb.com/docs">here</a>.</p>
+                <p>This site is powered by the <a href="https://github.com/rotexsoft/slim3-skeleton-mvc-app">SlimPHP 3 Skeleton MVC App.</a></p>
             </div>
         </div>
         <div class="row">
@@ -66,9 +65,7 @@
             </div> 
         </footer>
 
-        <script src="<?php echo s3MVC_MakeLink('/js/foundation/vendor/jquery.js'); ?>"></script>
-        <script src="<?php echo s3MVC_MakeLink('/js/foundation/vendor/what-input.js'); ?>"></script>
-        <script src="<?php echo s3MVC_MakeLink('/js/foundation/vendor/foundation.min.js'); ?>"></script>
+        <script src="<?php echo s3MVC_MakeLink('/js/app.js'); ?>"></script>
         <script> $(document).foundation(); </script>
     </body>
 </html>
