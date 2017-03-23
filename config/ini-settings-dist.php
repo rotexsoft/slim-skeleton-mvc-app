52e755609a68c9fe3291b7b243115cbac8514a75
Rotimi Ade <rotexdegba007-github@yahoo.ca>
2017-03-23 14:16:31 -0600

    Added set_error_handler to ./config/ini-settings-dist.php to convert php errors to exceptions.

--- config/ini-settings-dist.php
+++ config/ini-settings-dist.php
@@ -12,3 +12,14 @@ if( s3MVC_GetCurrentAppEnvironment() !== S3MVC_APP_ENV_DEV ) {
     
     ini_set('error_reporting',  E_ALL & ~E_NOTICE & ~E_STRICT & ~E_DEPRECATED  & ~E_WARNING);
 }
+
+set_error_handler(function ($severity, $message, $file, $line) {
+    
+    if (!(error_reporting() & $severity)) {
+        // This error code is not included in error_reporting, so ignore it
+        return;
+    }
+    
+    // convert php errors to exception
+    throw new \ErrorException($message, 0, $severity, $file, $line);
+});
