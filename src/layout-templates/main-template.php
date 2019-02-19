aac3bc86d6e4980bfe1dc7ecd589cfc43e268657
rotexdegba <rotimi@w-edm-192-139-007-131.nofc.cfs.nrcan.gc.ca>
2019-02-19 14:04:21 -0700

    Updated the foundation UI package to version 6.5.1

--- src/layout-templates/main-template.php
+++ src/layout-templates/main-template.php
@@ -9,38 +9,38 @@
         <script src="<?php echo s3MVC_MakeLink('/js/foundation/vendor/jquery.js'); ?>"></script>
     </head>
     <body>
-        <div class="row">
-            <div class="small-12 columns">
-                <ul class="menu" style="padding-left: 0;">
-                    <li><a href="#">Section 1</a></li>
-                    <li><a href="#">Section 2</a></li>
-                    <li><a href="#">Section 3</a></li>
-                </ul>
-            </div>
-        </div>
-        <div class="row">
-            <div class="small-12 columns">
-                <h1>Welcome to Your New Site</h1>
-                <p>This site is powered by the <a href="https://github.com/rotexsoft/slim3-skeleton-mvc-app">SlimPHP 3 Skeleton MVC App Micro-Framework</a> based on SlimPHP 3. It also ships with the <a href="http://foundation.zurb.com/">Foundation</a> UI framework. Everything you need to know about using the Foundation UI framework can be found <a href="http://foundation.zurb.com/docs">here</a>.</p>
-            </div>
-        </div>
-
-        <div class="row">    
-            <div class="small-12 columns">
-                <?php echo $content; ?>                
-            </div>
-        </div>
-
-        <footer class="row">
-            <div class="small-12 columns">
-                <hr/>
-                <div class="row">
-                    <div class="small-6 columns">
-                        <p>© Copyright no one at all. Go to town.</p>
-                    </div>
+        <div class="grid-container">     
+            <div class="grid-x  grid-padding-x">
+                <div class="small-12 cell">
+                    <ul class="menu" style="padding-left: 0;">
+                        <li><a href="#">Section 1</a></li>
+                        <li><a href="#">Section 2</a></li>
+                        <li><a href="#">Section 3</a></li>
+                    </ul>
                 </div>
-            </div> 
-        </footer>
+            </div>
+            <div class="grid-x grid-padding-x">
+                <div class="small-12 cell">
+                    <h1>Welcome to Your New Site</h1>
+                    <p>This site is powered by the <a href="https://github.com/rotexsoft/slim3-skeleton-mvc-app">SlimPHP 3 Skeleton MVC App Micro-Framework</a> based on SlimPHP 3. It also ships with the <a href="http://foundation.zurb.com/">Foundation</a> UI framework. Everything you need to know about using the Foundation UI framework can be found <a href="http://foundation.zurb.com/docs">here</a>.</p>
+                </div>
+            </div>
+            <div class="grid-x grid-padding-x">    
+                <div class="small-12 cell">
+                    <?php echo $content; ?>                
+                </div>
+            </div>
+            <footer class="grid-x grid-padding-x">
+                <div class="small-12 cell">
+                    <hr/>
+                    <div class="grid-x">
+                        <div class="small-6 cell">
+                            <p>© Copyright no one at all. Go to town.</p>
+                        </div>
+                    </div>
+                </div> 
+            </footer>
+        </div><!-- <div class="grid-container"> -->
 
         <script src="<?php echo s3MVC_MakeLink('/js/foundation/vendor/what-input.js'); ?>"></script>
         <script src="<?php echo s3MVC_MakeLink('/js/foundation/vendor/foundation.min.js'); ?>"></script>
