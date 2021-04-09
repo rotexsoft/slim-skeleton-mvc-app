4c451aa964bebf34305893f9ac037a40bcb03e8f
Rotimi Ade <rotexdegba007-github@yahoo.ca>
2021-04-09 01:33:20 -0600

    Preparing for slim 4 upgrade

--- public/index-dist.php
+++ public/index-dist.php
@@ -1,52 +1,52 @@
 <?php
 require dirname(__FILE__, 2) . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
 
-define('S3MVC_APP_ENV_DEV', 'development');
-define('S3MVC_APP_ENV_PRODUCTION', 'production');
-define('S3MVC_APP_ENV_STAGING', 'staging');
-define('S3MVC_APP_ENV_TESTING', 'testing');
+define('SMVC_APP_ENV_DEV', 'development');
+define('SMVC_APP_ENV_PRODUCTION', 'production');
+define('SMVC_APP_ENV_STAGING', 'staging');
+define('SMVC_APP_ENV_TESTING', 'testing');
 
-define('S3MVC_APP_PUBLIC_PATH', dirname(__FILE__));
-define('S3MVC_APP_ROOT_PATH', dirname(dirname(__FILE__)));
+define('SMVC_APP_PUBLIC_PATH', dirname(__FILE__));
+define('SMVC_APP_ROOT_PATH', dirname(dirname(__FILE__)));
 
 // If true, the mvc routes will be enabled. If false, then you must explicitly
 // define all the routes for your application inside config/routes-and-middlewares.php
-define('S3MVC_APP_USE_MVC_ROUTES', true);
+define('SMVC_APP_USE_MVC_ROUTES', true);
 
 // If true, the string `action` will be prepended to action method names (if the
 // method name does not already start with the string `action`). The resulting
 // method name will be converted to camel case before being executed.
 // If false, then action method names will only be converted to camel
 // case before being executed.
-// NOTE: This setting does not apply to S3MVC_APP_DEFAULT_ACTION_NAME.
+// NOTE: This setting does not apply to SMVC_APP_DEFAULT_ACTION_NAME.
 //       It only applies to the routes below:
 //          '/{controller}/{action}[/{parameters:.+}]'
 //          '/{controller}/{action}/'
-define('S3MVC_APP_AUTO_PREPEND_ACTION_TO_ACTION_METHOD_NAMES', false);
+define('SMVC_APP_AUTO_PREPEND_ACTION_TO_ACTION_METHOD_NAMES', false);
 
 // This is used to create a controller object to handle the default / route.
 // Must be prefixed with the namespace if the controller class is in a namespace.
-define('S3MVC_APP_DEFAULT_CONTROLLER_CLASS_NAME', \Slim3MvcTools\Controllers\BaseController::class);
+define('SMVC_APP_DEFAULT_CONTROLLER_CLASS_NAME', \SlimMvcTools\Controllers\BaseController::class);
 
 // This is the name of the action / method to be called on the default controller
 // to handle the default / route. This method should return a response string (ie.
 // valid html) or a PSR 7 response object containing valid html in its body.
 // This default action / method should accept no arguments / parameters.
-define('S3MVC_APP_DEFAULT_ACTION_NAME', 'actionIndex');
+define('SMVC_APP_DEFAULT_ACTION_NAME', 'actionIndex');
 
-s3MVC_GetSuperGlobal(); // this method is first called here to ensure that $_SERVER,
+sMVC_GetSuperGlobal(); // this method is first called here to ensure that $_SERVER,
                         // $_GET, $_POST, $_FILES, $_COOKIE, $_SESSION & $_ENV are
                         // captured in their original state by the static $super_globals
-                        // variable inside s3MVC_GetSuperGlobal(), before any other
+                        // variable inside sMVC_GetSuperGlobal(), before any other
                         // library, framework, etc. accesses or modifies any of them.
-                        // Subsequent calls to s3MVC_GetSuperGlobal(..) will return
+                        // Subsequent calls to sMVC_GetSuperGlobal(..) will return
                         // the stored values.
 /**
  * @param string $error_message A brief description of the message
  * @param string $file_path path to the missing file
  * @param string $dist_file_path path to the dist file that can be used to create the missing file
  */
-function s3MVC_DisplayAndLogFrameworkFileNotFoundError($error_message, $file_path, $dist_file_path) {
+function sMVC_DisplayAndLogFrameworkFileNotFoundError($error_message, $file_path, $dist_file_path) {
 
     $file_missing_error_page = <<<END
 <html>
@@ -78,7 +78,7 @@ function s3MVC_DisplayAndLogFrameworkFileNotFoundError($error_message, $file_pat
 END;
     echo $file_missing_error_page;
 
-    $current_uri = \Slim\Http\Request::createFromEnvironment(new \Slim\Http\Environment(s3MVC_GetSuperGlobal('server')))->getUri()->__toString();
+    $current_uri = \Slim\Http\Request::createFromEnvironment(new \Slim\Http\Environment(sMVC_GetSuperGlobal('server')))->getUri()->__toString();
 
     // Write full message to log via error_log(...)
     // http://php.net/manual/en/function.error-log.php
@@ -86,11 +86,11 @@ END;
         . " Please copy `$dist_file_path` to `$file_path` and"
         . " configure `$file_path` for your application's current environment.";
 
-    $s3mvc_logger_for_startup_errors = function () {
+    $smvc_logger_for_startup_errors = function () {
 
         $ds = DIRECTORY_SEPARATOR;
         $log_type = \Vespula\Log\Adapter\ErrorLog::TYPE_FILE;
-        $file = S3MVC_APP_ROOT_PATH . "{$ds}logs{$ds}daily_log_" . date('Y_M_d') . '.txt';
+        $file = SMVC_APP_ROOT_PATH . "{$ds}logs{$ds}daily_log_" . date('Y_M_d') . '.txt';
 
         $adapter = new \Vespula\Log\Adapter\ErrorLog($log_type , $file);
         $adapter->setMessageFormat('[{timestamp}] [{level}] {message}');
@@ -99,7 +99,7 @@ END;
 
         return new \Vespula\Log\Log($adapter);
     };
-    $s3mvc_logger_for_startup_errors()->error($log_message); // log to log file
+    $smvc_logger_for_startup_errors()->error($log_message); // log to log file
 
     // error_log ( $log_message , 0 ) means message is sent to PHP's system logger,
     // using the Operating System's system logging mechanism or a file, depending
@@ -116,25 +116,25 @@ END;
  * This function detects which environment your web-app is running in
  * (i.e. one of Production, Development, Staging or Testing).
  *
- * NOTE: Make sure you edit ../config/env.php to return one of S3MVC_APP_ENV_DEV,
- *       S3MVC_APP_ENV_PRODUCTION, S3MVC_APP_ENV_STAGING or S3MVC_APP_ENV_TESTING
+ * NOTE: Make sure you edit ../config/env.php to return one of SMVC_APP_ENV_DEV,
+ *       SMVC_APP_ENV_PRODUCTION, SMVC_APP_ENV_STAGING or SMVC_APP_ENV_TESTING
  *       relevant to the environment you are installing your web-app.
  *
  * @return string
  */
-function s3MVC_GetCurrentAppEnvironment() {
+function sMVC_GetCurrentAppEnvironment() {
 
     static $current_env;
 
     if( !$current_env ) {
 
-        $root_dir = S3MVC_APP_ROOT_PATH. DIRECTORY_SEPARATOR;
+        $root_dir = SMVC_APP_ROOT_PATH. DIRECTORY_SEPARATOR;
         $env_file_path = $root_dir.'config'. DIRECTORY_SEPARATOR.'env.php';
 
         if( !file_exists($env_file_path) ) {
 
             $env_dist_file_path = "{$root_dir}config". DIRECTORY_SEPARATOR.'env-dist.php';
-            s3MVC_DisplayAndLogFrameworkFileNotFoundError(
+            sMVC_DisplayAndLogFrameworkFileNotFoundError(
                 'Missing Environment Configuration File Error',
                 $env_file_path,
                 $env_dist_file_path
@@ -149,7 +149,7 @@ function s3MVC_GetCurrentAppEnvironment() {
     return $current_env;
 }
 
-function s3MVC_PrependAction2ActionMethodName($action_method_name) {
+function sMVC_PrependAction2ActionMethodName($action_method_name) {
 
     if( strtolower( substr($action_method_name, 0, 6) ) !== "action"){
 
@@ -159,18 +159,18 @@ function s3MVC_PrependAction2ActionMethodName($action_method_name) {
     return $action_method_name;
 }
 
-$s3mvc_root_dir = S3MVC_APP_ROOT_PATH. DIRECTORY_SEPARATOR;
+$smvc_root_dir = SMVC_APP_ROOT_PATH. DIRECTORY_SEPARATOR;
 
 // handle ini settings
-require_once "{$s3mvc_root_dir}config". DIRECTORY_SEPARATOR.'ini-settings.php';
+require_once "{$smvc_root_dir}config". DIRECTORY_SEPARATOR.'ini-settings.php';
 
 
-$app_settings_file_path = "{$s3mvc_root_dir}config". DIRECTORY_SEPARATOR.'app-settings.php';
+$app_settings_file_path = "{$smvc_root_dir}config". DIRECTORY_SEPARATOR.'app-settings.php';
 
 if( !file_exists($app_settings_file_path) ) {
 
-    $app_settings_dist_file_path = "{$s3mvc_root_dir}config". DIRECTORY_SEPARATOR.'app-settings-dist.php';
-    s3MVC_DisplayAndLogFrameworkFileNotFoundError(
+    $app_settings_dist_file_path = "{$smvc_root_dir}config". DIRECTORY_SEPARATOR.'app-settings-dist.php';
+    sMVC_DisplayAndLogFrameworkFileNotFoundError(
         'Missing App Settings Configuration File Error',
         $app_settings_file_path,
         $app_settings_dist_file_path
@@ -188,13 +188,13 @@ $container = $app->getContainer();
 //        Add objects to the dependency injection container
 ////////////////////////////////////////////////////////////////////////////////
 
-require_once "{$s3mvc_root_dir}config". DIRECTORY_SEPARATOR.'dependencies.php';
+require_once "{$smvc_root_dir}config". DIRECTORY_SEPARATOR.'dependencies.php';
 
 ////////////////////////////////////////////////////////////////////////////////
 // End Dependency Injection Configuration
 ////////////////////////////////////////////////////////////////////////////////
 
-$s3mvc_default_route_handler =
+$smvc_default_route_handler =
 function (
     \Psr\Http\Message\ServerRequestInterface $request,
     \Psr\Http\Message\ResponseInterface $response,
@@ -208,17 +208,17 @@ function (
 
     // No controller or action was specified, so use default controller and
     // invoke the default action on it.
-    $default_action = S3MVC_APP_DEFAULT_ACTION_NAME;
-    $default_controller = S3MVC_APP_DEFAULT_CONTROLLER_CLASS_NAME;
+    $default_action = SMVC_APP_DEFAULT_ACTION_NAME;
+    $default_controller = SMVC_APP_DEFAULT_CONTROLLER_CLASS_NAME;
 
     $default_controller_parts = explode('\\', $default_controller);
-    $default_controller_from_uri = \Slim3MvcTools\Functions\Str\toDashes(array_pop($default_controller_parts));
+    $default_controller_from_uri = \SlimMvcTools\Functions\Str\toDashes(array_pop($default_controller_parts));
 
     // create default controller
     $default_controller_obj = new $default_controller(
                                         $this,
                                         $default_controller_from_uri,
-                                        \Slim3MvcTools\Functions\Str\toDashes($default_action),
+                                        \SlimMvcTools\Functions\Str\toDashes($default_action),
                                         $request, $response
                                     );
 
@@ -241,7 +241,7 @@ function (
     return $default_controller_obj->postAction($response);
 };
 
-$s3mvc_route_handler =
+$smvc_route_handler =
 function(
     \Psr\Http\Message\ServerRequestInterface $req,
     \Psr\Http\Message\ResponseInterface $resp,
@@ -253,11 +253,11 @@ function(
     //       It is automatically bound to this closure by Slim 3 when any of
     //       $app->map or $app->get or $app->post, etc is called.
     $container = $this;
-    $action_method = \Slim3MvcTools\Functions\Str\dashesToCamel($args['action']);
+    $action_method = \SlimMvcTools\Functions\Str\dashesToCamel($args['action']);
 
-    if( S3MVC_APP_AUTO_PREPEND_ACTION_TO_ACTION_METHOD_NAMES ) {
+    if( SMVC_APP_AUTO_PREPEND_ACTION_TO_ACTION_METHOD_NAMES ) {
 
-        $action_method = s3MVC_PrependAction2ActionMethodName($action_method);
+        $action_method = sMVC_PrependAction2ActionMethodName($action_method);
     }
 
     // strip trailing forward slash
@@ -283,10 +283,10 @@ function(
         return $notFoundHandler($req, $resp, null, $log_message);
     }
 
-    $controller_obj = s3MVC_CreateController($this, $args['controller'], $args['action'], $req, $resp);
+    $controller_obj = sMVC_CreateController($this, $args['controller'], $args['action'], $req, $resp);
 
     if(
-        $controller_obj instanceof \Slim3MvcTools\Controllers\BaseController
+        $controller_obj instanceof \SlimMvcTools\Controllers\BaseController
         && !method_exists($controller_obj, $action_method)
     ) {
         $controller_class_name = get_class($controller_obj);
@@ -300,7 +300,7 @@ function(
         return $notFoundHandler($req, $resp, null, $log_message);
 
     } else if (
-        $controller_obj instanceof \Slim3MvcTools\Controllers\BaseController
+        $controller_obj instanceof \SlimMvcTools\Controllers\BaseController
     ) {
         $pre_action_response = $controller_obj->preAction();
         $controller_obj->setResponse( $pre_action_response );
@@ -324,7 +324,7 @@ function(
 
     } else {
 
-        // s3MVC_CreateController(..) returned a Response object containing a
+        // sMVC_CreateController(..) returned a Response object containing a
         // not found page.
         $resp = $controller_obj;
     }
@@ -332,7 +332,7 @@ function(
     return $resp;
 };
 
-$s3mvc_controller_only_route_handler =
+$smvc_controller_only_route_handler =
 function (
     \Psr\Http\Message\ServerRequestInterface $req,
     \Psr\Http\Message\ResponseInterface $resp,
@@ -346,17 +346,17 @@ function (
 
     // No action was specified, so invoke the default action on specified
     // controller.
-    $default_action = S3MVC_APP_DEFAULT_ACTION_NAME;
+    $default_action = SMVC_APP_DEFAULT_ACTION_NAME;
 
-    // s3MVC_CreateController could return a Response object if $args['controller']
+    // sMVC_CreateController could return a Response object if $args['controller']
     // doesn't match any existing controller class.
     $controller_object =
-        s3MVC_CreateController(
-            $this, $args['controller'], \Slim3MvcTools\Functions\Str\toDashes($default_action), $req, $resp
+        sMVC_CreateController(
+            $this, $args['controller'], \SlimMvcTools\Functions\Str\toDashes($default_action), $req, $resp
         );
 
     if(
-        $controller_object instanceof \Slim3MvcTools\Controllers\BaseController
+        $controller_object instanceof \SlimMvcTools\Controllers\BaseController
         && !method_exists($controller_object, $default_action)
     ) {
         $controller_class_name = get_class($controller_object);
@@ -370,7 +370,7 @@ function (
         return $notFoundHandler($req, $resp, null, $log_message);
 
     }  else if (
-        $controller_object instanceof \Slim3MvcTools\Controllers\BaseController
+        $controller_object instanceof \SlimMvcTools\Controllers\BaseController
     ) {
         $pre_action_response = $controller_object->preAction();
         $controller_object->setResponse( $pre_action_response );
@@ -393,7 +393,7 @@ function (
 
     } else {
 
-        // s3MVC_CreateController(..) returned a Response object containing a
+        // sMVC_CreateController(..) returned a Response object containing a
         // not found page.
         $resp = $controller_object;
     }
@@ -405,7 +405,7 @@ function (
 // Start: Load app specific routes
 ////////////////////////////////////////////////////////////////////////////////
 
-require_once "{$s3mvc_root_dir}config". DIRECTORY_SEPARATOR.'routes-and-middlewares.php';
+require_once "{$smvc_root_dir}config". DIRECTORY_SEPARATOR.'routes-and-middlewares.php';
 
 ////////////////////////////////////////////////////////////////////////////////
 // End: Load app specific routes
@@ -415,17 +415,17 @@ require_once "{$s3mvc_root_dir}config". DIRECTORY_SEPARATOR.'routes-and-middlewa
 // Start: mvc routes
 /////////////////////////////
 
-if( S3MVC_APP_USE_MVC_ROUTES ) {
+if( SMVC_APP_USE_MVC_ROUTES ) {
 
     // default route
-    $app->map( ['GET', 'POST'], '/', $s3mvc_default_route_handler );
+    $app->map( ['GET', 'POST'], '/', $smvc_default_route_handler );
 
     // controller with no action and params route handler
-    $app->map(['GET', 'POST'], '/{controller}[/]', $s3mvc_controller_only_route_handler);
+    $app->map(['GET', 'POST'], '/{controller}[/]', $smvc_controller_only_route_handler);
 
     // controller with action and optional params route handler
-    $app->map([ 'GET', 'POST' ], '/{controller}/{action}[/{parameters:.+}]', $s3mvc_route_handler);
-    $app->map([ 'GET', 'POST' ], '/{controller}/{action}/', $s3mvc_route_handler);//handle trailing slash
+    $app->map([ 'GET', 'POST' ], '/{controller}/{action}[/{parameters:.+}]', $smvc_route_handler);
+    $app->map([ 'GET', 'POST' ], '/{controller}/{action}/', $smvc_route_handler);//handle trailing slash
 }
 /////////////////////////////
 // End: mvc routes
