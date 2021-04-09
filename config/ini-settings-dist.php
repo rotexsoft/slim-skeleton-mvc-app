52f9f5641b70b26c2fdb884955bb7449aa32d83d
Rotimi Ade <rotexdegba007-github@yahoo.ca>
2021-04-09 01:16:26 -0600

    Preparing for slim 4 upgrade

--- config/ini-settings-dist.php
+++ config/ini-settings-dist.php
@@ -8,7 +8,7 @@ ini_set('display_errors', true);
 ini_set('html_errors', true);
 ini_set('date.timezone', 'America/Edmonton');
 
-if( s3MVC_GetCurrentAppEnvironment() !== S3MVC_APP_ENV_DEV ) {
+if( sMVC_GetCurrentAppEnvironment() !== SMVC_APP_ENV_DEV ) {
     
     ini_set('error_reporting',  E_ALL & ~E_NOTICE & ~E_STRICT & ~E_DEPRECATED  & ~E_WARNING);
 }
