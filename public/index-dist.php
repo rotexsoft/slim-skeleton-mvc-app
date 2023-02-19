17a99d631491ca01158657cf63c4dc900ca82c6d
Rotimi Ade <rotexdegba007-github@yahoo.ca>
2023-02-18 22:34:29 -0700

    Slim4 prep

--- public/index-dist.php
+++ public/index-dist.php
@@ -1,4 +1,6 @@
 <?php
+declare(strict_types=1);
+
 require dirname(__FILE__, 2) . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
 
 ////////////////////////////////////////////////////////////////////////////////
