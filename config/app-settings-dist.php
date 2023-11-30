5ddf0345d076e5eb9b5849c2ef4625aff58c4dea
rotimi <rotimi@localhost>
2023-11-30 14:44:51 -0700

    Testing & refactoring in progress

--- config/app-settings-dist.php
+++ config/app-settings-dist.php
@@ -56,6 +56,20 @@ return [
     'html_renderer_class' => \SlimMvcTools\HtmlErrorRenderer::class,
     'log_renderer_class'  => \SlimMvcTools\LogErrorRenderer::class,
     
+    'base_controller_action_login_empty_username_msg' => "The 'username' field is empty.",
+    'base_controller_action_login_empty_password_msg' => "The 'password' field is empty.",
+    'base_controller_do_login_auth_is_valid_msg' => 'You are now logged into a new session.',
+    'base_controller_do_login_auth_not_is_valid_msg' => 'Login Failed!',
+    'base_controller_do_login_auth_v_auth_exception_general_msg' => 'Login Failed!<br>Please contact site administrator or try again later.',
+    'base_controller_do_login_auth_v_auth_exception_back_end_msg' => "Login Failed!<br>Can't connect to login server right now.<br>Please try again later.",
+    'base_controller_do_login_auth_v_auth_exception_user_passwd_msg' => 'Login Failed!<br>Incorrect User Name and Password combination.<br>Please try again.',
+    'base_controller_do_login_auth_exception_msg' => 'Login Failed!<br>Please contact the site administrator.',
+    'base_controller_action_login_status_is_anon_msg' => 'You are not logged in.',
+    'base_controller_action_login_status_is_idle_msg' => 'Your session was idle for too long. Please log in again.',
+    'base_controller_action_login_status_is_expired_msg' => 'Your session has expired. Please log in again.',
+    'base_controller_action_login_status_is_valid_msg' => 'You are still logged in.',
+    'base_controller_action_login_status_unknown_msg' => 'Unknown session status.',
+    
     // add other stuff like DB credentials, api keys, etc below
     
     ////////////////////////////////////////////////////
