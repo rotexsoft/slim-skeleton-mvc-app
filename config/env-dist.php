0182c9898818446a011144ebc8b9f1ce82831e02
rotimi <rotimi@localhost>
2024-05-25 22:34:19 -0600

    Added mkdocs

--- config/env-dist.php
+++
@@ -1,6 +0,0 @@
-<?php
-/**
- * Return one of SMVC_APP_ENV_DEV, SMVC_APP_ENV_PRODUCTION, SMVC_APP_ENV_STAGING 
- * or SMVC_APP_ENV_TESTING relevant to the environment you are installing your web-app.
- */
-return SMVC_APP_ENV_DEV;
--- tests/bootstrap.php
+++
@@ -1,4 +0,0 @@
-<?php
-error_reporting(E_ALL);
-
-require_once dirname(__DIR__).DIRECTORY_SEPARATOR.'vendor/autoload.php';
